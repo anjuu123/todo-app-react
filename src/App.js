a9b1c4e80db9b3e6cb262533fb0a9d36e48901b1
Anju Dhungana <anjudhungana28@gmail.com>
2023-03-16 18:13:14 -0400

    import signup from signup component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 
 import './App.css';
+import Signup from './Component/Signup';
 
 function App() {
   return (
     <div>
-    <h2>Sign Up</h2>
-    <form>
-      <label>
-        Name:
-        <input type="text"  />
-      </label>
-      <label>
-        Email:
-        <input type="email" />
-      </label>
-      <label>
-        Password:
-        <input type="password" />
-      </label>
-      <label>
-        Confirm Password:
-        <input type="cpassword" />
-      </label>
-      <button type="submit">Sign Up</button>
-    </form>
+    <Signup />
   </div>
   )
 }
