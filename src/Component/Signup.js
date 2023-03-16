9254324aa34c1d3a5facfc6bedc821e99197f2b5
Anju Dhungana <anjudhungana28@gmail.com>
2023-03-16 18:11:07 -0400

    create seperate component file for signup

---
+++ src/Component/Signup.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Signup = ()=> {
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form>
+        <label>
+          Name:
+          <input type="text" placeholder="enter your name" />
+        </label>
+
+        <label>
+          Email:
+          <input type="email" placeholder="enter your email" />
+        </label>
+
+        <label>
+          Password:
+          <input type="password" placeholder="enter password" />
+        </label>
+        <label>
+          Confirm Password:
+          <input type="cpassword" placeholder="confirm password" />
+        </label>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+}
