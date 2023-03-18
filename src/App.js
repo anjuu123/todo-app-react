5667b75358727627fa19ae959ecec1279030c048
Anju Dhungana <anjudhungana28@gmail.com>
2023-03-18 19:38:40 -0400

    update code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-
 import './App.css';
-import Signup from './Component/Signup';
+import React, { useState } from "react";
+import {Login } from './Component/Login';
+import { Signup } from './Component/Signup';
 
 function App() {
+  const [showSignup, setShowSignup] = useState(false);
+
+  // when button is clicked function is triggred
+  const toggleForm = () => {
+    setShowSignup(!showSignup);
+  };
+
   return (
     <div>
-    <Signup />
-  </div>
-  )
+      {/* showSignup is true then signup component is rendered else login is rendered */}
+      {showSignup ? <Signup /> : <Login />}
+      <button onClick={toggleForm}>
+        {showSignup ? "Already have an account? SignIn" : "Don't have an account? Create here"}
+      </button>
+    </div>
+  );
 }
 
 export default App;
